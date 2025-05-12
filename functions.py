2305d113040fe08d1b15555578ac09384df5391f
kainat224 <anmolkainat244@gmail.com>
2025-05-13 03:55:02 +0500

    functions done

--- functions.py
+++ functions.py
@@ -14,9 +14,32 @@
 # greater_num()
 
 # Task 2 - Celsius to Fahrenheit 
-temp_celsius = int(input("Enter temperature: "))
-def celsius_to_fahrenheit():
-    temp_fahrenheit = temp_celsius * 9/5 + 32
-    print("Temperature in Fahrenheit: ", temp_fahrenheit)
+# def celsius_to_fahrenheit(temp_celsius):
+#     temp_fahrenheit = temp_celsius * 9/5 + 32
+#     print("Temperature in Fahrenheit: ", round(temp_fahrenheit, 2), "°F")
 
-celsius_to_fahrenheit()
+# temp_celsius = int(input("Enter temperature: "))
+# celsius_to_fahrenheit(temp_celsius)
+
+# Task 3 - recursive function
+# def sum():
+#     if(n == 1):
+#         return 1
+#     return sum(n-1) + n
+
+# print(sum(4)) 
+
+# Task 4
+# def printing_pattern(n):
+#     for i in range(n, 0, -1):
+#         print("*"*i)
+
+# n = int(input("Enter number: "))
+# printing_pattern(n)
+
+# Task 5 - convert inches to cms
+def inches_to_cms(n):
+    return (f"{n * 2.54} cm")
+
+n = int(input("Enter Num: "))
+print(inches_to_cms(n))
